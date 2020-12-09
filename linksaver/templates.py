c5616e57b3bc1017769bf617c3b3829d04f27dc0
Alexander Richey <alexander.richey@gmail.com>
2020-12-09 01:20:42 -0500

    fix xss stuff

--- linksaver/templates.py
+++ linksaver/templates.py
@@ -1,8 +1,39 @@
 from starlette.templating import Jinja2Templates
 import jinja2
 import markdown
+import html
+import bleach
 
-md = markdown.Markdown(extensions=["markdown.extensions.fenced_code"])
+allowed_tags = [
+    "a",
+    "abbr",
+    "acronym",
+    "b",
+    "blockquote",
+    "code",
+    "em",
+    "i",
+    "li",
+    "ol",
+    "pre",
+    "strong",
+    "ul",
+    "h1",
+    "h2",
+    "h3",
+    "p",
+    "br",
+    "ins",
+    "del",
+]
 
 templates = Jinja2Templates(directory="templates")
-templates.env.filters["markdown"] = lambda text: jinja2.Markup(md.convert(text))
+templates.env.filters["markdown"] = lambda text: jinja2.Markup(
+    bleach.clean(
+        markdown.markdown(
+            text,
+            extensions=["markdown.extensions.fenced_code"],
+        ),
+        tags=allowed_tags,
+    )
+)
